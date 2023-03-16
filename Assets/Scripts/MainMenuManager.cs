f099125a88671e1ff8065a89495ccb80111202eb
wtsiwon <siwon4727@gmail.com>
2023-03-16 09:44:23 +0900

    MainMenuUI

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Button gameStartBtn;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
