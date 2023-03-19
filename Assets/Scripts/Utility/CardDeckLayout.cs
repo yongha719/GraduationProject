26e63eef7a0b21fa16f4d2656463ca8c59ccd923
Yongha <loha31707@gmail.com>
2023-03-20 00:56:31 +0900

    [용하] 버그에반데..

--- Assets/Scripts/Utility/CardDeckLayout.cs
+++ Assets/Scripts/Utility/CardDeckLayout.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [AddComponentMenu("MyComponent/CardLayout", int.MinValue)]
 public class CardDeckLayout : MonoBehaviour
 {
+    Vector3 distance = new Vector3(300, 0, 0);
+    Quaternion leftRotation = Quaternion.Euler(new Vector3(0, 0, 15));
+    Quaternion rightRotation = Quaternion.Euler(new Vector3(0, 0, -15));
+
     private void OnTransformChildrenChanged()
     {
         float[] lerpValue = new float[transform.childCount];
@@ -27,20 +31,22 @@ public class CardDeckLayout : MonoBehaviour
                 break;
         }
 
-        Vector3 distance = new Vector3(400, 0, 0);
-        Quaternion leftRotation = Quaternion.Euler(new Vector3(0, 0, 15));
-        Quaternion rightRotation = Quaternion.Euler(new Vector3(0, 0, -15));
         for (int i = 0; i < transform.childCount; i++)
         {
             Vector3 targetPos = Vector3.Lerp(-distance, distance, lerpValue[i]);
             Quaternion targetRos = Quaternion.identity;
+
             if (transform.childCount >= 4)
             {
+                print(transform.childCount);
                 float curve = Mathf.Sqrt(Mathf.Pow(0.5f, 2) - Mathf.Pow(lerpValue[i] - 0.5f, 2));
                 targetPos.y += curve;
                 targetRos = Quaternion.Slerp(leftRotation, rightRotation, lerpValue[i]);
             }
+
             RectTransform rect = transform.GetChild(i) as RectTransform;
+            print(rect.name);
+            print(targetPos);
             rect.anchoredPosition = targetPos;
             rect.localRotation = targetRos;
         }
