af2a14bca11ba9396edc373cd7c4b57400915cd2
Yongha <loha31707@gmail.com>
2023-04-18 16:04:58 +0900

    PhotonManager & CardManager
    Create ICard
    Create PhotonManager

--- Assets/Scripts/Utility/CanvasUtility.cs
+++ Assets/Scripts/Utility/CanvasUtility.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 이 스크립트는 Canvas에 있어야 합니다
-/// </summary>
+/// <summary> 이 스크립트는 Canvas에 있어야 합니다 </summary>
 public class CanvasUtility : MonoBehaviour
 {
     private static Vector2 screenPoint;
@@ -21,9 +19,7 @@ public class CanvasUtility : MonoBehaviour
         CanvasTr = transform as RectTransform;
     }
 
-    /// <summary>
-    ///  - 현재 마우스 위치를 캔버스 좌표 기준으로 가져오는 함수
-    /// </summary>
+    /// <summary> - 현재 마우스 위치를 캔버스 좌표 기준으로 가져오는 함수 </summary>
     public static Vector2 GetMousePosToCanvasPos()
     {
         RectTransformUtility.ScreenPointToLocalPointInRectangle(CanvasTr, Input.mousePosition, Camera.main, out screenPoint);
@@ -31,9 +27,7 @@ public class CanvasUtility : MonoBehaviour
         return screenPoint;
     }
 
-    /// <summary>
-    ///  - World Position을 캔버스 좌표 기준으로 가져오는 함수
-    /// </summary>
+    /// <summary> - World Position을 캔버스 좌표 기준으로 가져오는 함수</summary>
     public static Vector2 GetWorldPosToCanvasPos(Vector3 pos)
     {
         RectTransformUtility.ScreenPointToLocalPointInRectangle(CanvasTr, pos, Camera.main, out screenPoint);
@@ -41,17 +35,13 @@ public class CanvasUtility : MonoBehaviour
         return screenPoint;
     }
 
-    /// <returns> 
-    ///  - 상대 필드의 Bottom Left와 Top Right를 (Vector2, Vector2) 튜플로 반환
-    /// </returns>
+    /// <returns> 상대 필드의 Bottom Left와 Top Right를 (Vector2, Vector2) 튜플로 반환 </returns>
     public static (Vector2, Vector2) GetEnemyFieldPosition()
     {
         return (EnemyFieldBottomLeft, EnemyFieldBottomRight);
     }
 
-    /// <returns> 
-    ///  - 내 필드의 Bottom Left와 Top Right를 AnchorPosition (Vector2, Vector2) 튜플로 반환
-    /// </returns>
+    /// <returns> - 내 필드의 Bottom Left와 Top Right를 AnchorPosition (Vector2, Vector2) 튜플로 반환 </returns>
     public static (Vector2, Vector2) GetMyFieldPosition()
     {
         return (MyFieldBottomLeft, MyFieldBottomRight);
