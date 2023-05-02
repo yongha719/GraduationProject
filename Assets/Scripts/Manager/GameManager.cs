cd8e6d0e85a62cee2ba9c3e545f5239a177496ff
Yongha <loha31707@gmail.com>
2023-05-02 14:52:21 +0900

    구글 스프레드시트 연동

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,16 +1,52 @@
 using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.Networking;
 
 public class GameManager : SingletonPunCallbacks<GameManager>, IPunObservable
 {
     public string PlayerName;
 
+    public Dictionary<string, CardData> CardDatas = new Dictionary<string, CardData>();
+
+    private const string CARDDATA_URL = "https://docs.google.com/spreadsheets/d/1y26T7EyZe3DPMKAMjTs2PpUJ34UlDPmmy7k9oEy1Ye0/export?format=tsv&range=A5:I18";
+
     void Start()
     {
         print("Debug Test");
+
+        StartCoroutine(ERequsetCardData());
+    }
+
+    private IEnumerator ERequsetCardData()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(CARDDATA_URL);
+
+        yield return request.SendWebRequest();
+
+        ParsingCardData(request.downloadHandler.text);
+    }
+
+    void ParsingCardData(string requsetdata)
+    {
+        string[] line = requsetdata.Split('\n');
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            string[] data = line[i].Split('\t');
+
+            CardData cardData = new CardData(data);
+
+            string rating = data[8];
+            print(rating);
+
+            CardDatas.Add(rating, cardData);
+        }
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        
+
     }
 }
