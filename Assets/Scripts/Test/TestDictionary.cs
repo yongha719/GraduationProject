e499d3c9790cc353e5131aeaf53604ac211275a9
Yongha <loha31707@gmail.com>
2023-05-03 02:23:37 +0900

    Serialized Collection 에셋 추가

---
+++ Assets/Scripts/Test/TestDictionary.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDictionary : MonoBehaviour
+{
+    public Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+}
