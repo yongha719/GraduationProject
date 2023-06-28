1b95be8f2c8dcac0487f90bd5694ffa760dcd6b7
wtsiwon <siwon4727@gmail.com>
2023-06-29 00:25:37 +0900

    일러스트 연출 하나 만들어 봤음

--- Assets/Scripts/CharacterProduction.cs
+++ Assets/Scripts/CharacterProduction.cs
@@ -18,7 +18,8 @@ public class CharacterProduction : MonoBehaviour
     [SerializeField] private Vector2 endPos1;
 
     [SerializeField] private Vector2 startPos2;
-    [SerializeField] private Vector3 endPos2;
+    private Vector2 middleLerpPos = new Vector2(-0.5f, -0.5f);
+    [SerializeField] private Vector2 endPos2;
 
 
     private IEnumerator IPlay1()
@@ -26,29 +27,29 @@ public class CharacterProduction : MonoBehaviour
         StartCoroutine(IFadeInOut(darkBackGround, 0, 1, 0.2f));
 
         #region 화면 밖에서 화면중앙으로
-        StartCoroutine(ILerp(glitering, startPos1, middlePos, 0.1f));
-        StartCoroutine(ILerp(backGround, startPos1, middlePos, 0.1f));
+        StartCoroutine(ILerpRectTransform(glitering, startPos1, middlePos, 0.1f));
+        StartCoroutine(ILerpRectTransform(backGround, startPos1, middlePos, 0.1f));
 
         yield return new WaitForSeconds(0.1f);
 
-        StartCoroutine(ILerp(illustration, startPos1, middlePos, 0.1f));
+        StartCoroutine(ILerpRectTransform(illustration, startPos1, middlePos, 0.1f));
 
         yield return new WaitForSeconds(0.05f);
 
-        StartCoroutine(ILerp(illustration_back, startPos1, middlePos, 0.1f));
+        StartCoroutine(ILerpRectTransform(illustration_back, startPos1, middlePos, 0.1f));
         #endregion
         yield return new WaitForSeconds(2f);
 
         #region 화면중앙에서 화면 밖으로
-        StartCoroutine(ILerp(illustration, middlePos, endPos1, 0.1f));
-        StartCoroutine(ILerp(illustration_back, middlePos, endPos1, 0.1f));
+        StartCoroutine(ILerpRectTransform(illustration, middlePos, endPos1, 0.1f));
+        StartCoroutine(ILerpRectTransform(illustration_back, middlePos, endPos1, 0.1f));
 
         StartCoroutine(IFadeInOut(darkBackGround, 1, 0, 0.2f));
         yield return new WaitForSeconds(0.1f);
 
-        StartCoroutine(ILerp(glitering, middlePos, endPos1, 0.1f));
+        StartCoroutine(ILerpRectTransform(glitering, middlePos, endPos1, 0.1f));
 
-        StartCoroutine(ILerp(backGround, middlePos, endPos1, 0.1f));
+        StartCoroutine(ILerpRectTransform(backGround, middlePos, endPos1, 0.1f));
 
         #endregion
 
@@ -56,7 +57,7 @@ public class CharacterProduction : MonoBehaviour
         yield break;
     }
 
-    private IEnumerator ILerp(RectTransform origin, Vector3 startPos, Vector2 endPos, float time)
+    private IEnumerator ILerpRectTransform(RectTransform origin, Vector3 startPos, Vector2 endPos, float time)
     {
         float current = 0;
         float percent = 0;
@@ -76,6 +77,25 @@ public class CharacterProduction : MonoBehaviour
         yield break;
     }
 
+    private IEnumerator ILerpTransform(Transform origin, Vector3 startPos, Vector3 endPos, float time)
+    {
+        float current = 0;
+        float percent = 0;
+
+        Vector3 currentPos;
+
+        while (percent < 1)
+        {
+            current += Time.deltaTime;
+            percent = current / time;
+            currentPos = Vector2.Lerp(startPos, endPos, percent);
+
+            origin.position = currentPos;
+            yield return null;
+        }
+
+        yield break;
+    }
     private IEnumerator IFadeInOut(Image image, float startAlpha, float endAlpha, float time)
     {
         float current = 0;
@@ -83,7 +103,7 @@ public class CharacterProduction : MonoBehaviour
 
         Color fadeColor = default;
 
-        while(percent < 1) 
+        while(percent < 1)
         {
             current += Time.deltaTime;
             percent = current / time;
@@ -100,22 +120,18 @@ public class CharacterProduction : MonoBehaviour
 
     private IEnumerator IPlay2()
     {
-        yield return null;
-    }
+        StartCoroutine(ILerpTransform(transform, startPos2, middlePos, 0.1f));
+        yield return new WaitForSeconds(0.1f);
 
-    private IEnumerator ISlowLerp(RectTransform origin, Vector2 startPos,Vector2 endPos, float timeSpd, float time)
-    {
+        StartCoroutine(ILerpTransform(transform, middlePos, middleLerpPos, 2f));
+        yield return new WaitForSeconds(2f);
 
-
-        while(0 < time)
-        {
-
-            yield return null;
-        }
+        StartCoroutine(ILerpTransform(transform, middleLerpPos, endPos2, 0.1f));
+        yield return new WaitForSeconds(0.1f);
 
         yield break;
     }
-    
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
